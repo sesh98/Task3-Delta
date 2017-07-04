13220903f23e4f965d3d3ec398ff946d0af0e8d1
sesh98 <djokorox@gmail.com>
2017-07-04 08:03:09 +0530

    Create Signin.php

---
+++ Signin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form method="post" action="connect.php">
+Username<br><input type="text" name="user"><br><br>
+Password<br><input type="password" name="pass"><br><br>
+<input type="submit" name="reg" value="Register"><br><br>
+<input type="submit" name="signin" value="Sign-in">
+</form>
+</body>
+
+</html>
